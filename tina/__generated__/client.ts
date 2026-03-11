ee98dd14b96675a8ca658c82b268e473489d1b94
Eli Yarnelle <e.yarnelle@gmail.com>
2026-03-10 23:15:38 -0400

    Regenerate TinaCMS schema with active field skip cloud checks

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/eyarn/YarnelleFur-babies-website-tinacms/tina/__generated__/.cache/1773198709898', url: 'https://content.tinajs.io/2.2/content/dffd7077-227c-4847-92af-8032f3e0f98b/github/main', token: 'a12bf76a7cfbd16e500e486ba85364f0dd742494', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/eyarn/YarnelleFur-babies-website-tinacms/tina/__generated__/.cache/1773198871445', url: 'https://content.tinajs.io/2.2/content/dffd7077-227c-4847-92af-8032f3e0f98b/github/main', token: 'a12bf76a7cfbd16e500e486ba85364f0dd742494', queries,  });
 export default client;
   
