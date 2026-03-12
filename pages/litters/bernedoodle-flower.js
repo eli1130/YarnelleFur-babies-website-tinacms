1c66be1166695917338335741f6524dce616fc88
eli1130 <e.yarnelle@gmail.com>
2026-03-11 22:51:38 -0400

    Update bernedoodle-flower.js

--- pages/litters/bernedoodle-flower.js
+++ pages/litters/bernedoodle-flower.js
@@ -50,7 +50,7 @@ export default function Page(props) {
     damPhoto: cleanUrl(d.damPhoto) || FALLBACK,
     sirePhoto: cleanUrl(d.sirePhoto) || FALLBACK,
     photos,
-    prevPhotos: prevPhotos.length > 0 ? prevPhotos : [FALLBACK, FALLBACK, FALLBACK, FALLBACK],
+    prevPhotos: prevPhotos.length > 0 ? prevPhotos : [],
     puppies: (d.puppies || []).map(p => ({
       name: p.name,
       gender: p.gender,
