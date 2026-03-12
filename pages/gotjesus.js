414e2ca4840a34d151a1f2a27af4b4ea4653f2a8
eli1130 <e.yarnelle@gmail.com>
2026-03-12 19:50:51 -0400

    Update gotjesus.js

--- pages/gotjesus.js
+++ pages/gotjesus.js
@@ -98,6 +98,7 @@ export default function GotJesus() {
           <li><Link href="/gotjesus" className="active">Got Jesus?</Link></li>
           <li><Link href="/#litters">Available Litters</Link></li>
           <li><Link href="/reviews">Reviews</Link></li>
+          <li><Link href="/facility">Our Facility</Link></li>
           <li><Link href="/faq">FAQ &amp; Products</Link></li>
           <li className="nav-cta"><Link href="/#apply">Apply Now</Link></li>
         </ul>
