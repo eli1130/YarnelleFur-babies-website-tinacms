bac511c697fd5f6c5615d77527f408cb884eb968
eli1130 <e.yarnelle@gmail.com>
2026-03-18 17:40:16 -0400

    Create [...media].js

---
+++ pages/api/s3/[...media].js
@@ -0,0 +1,26 @@
+import { mediaHandlerConfig, createMediaHandler } from "next-tinacms-s3/dist/handlers";
+import { isAuthorized } from "@tinacms/auth";
+
+export const config = mediaHandlerConfig;
+
+export default createMediaHandler({
+  config: {
+    credentials: {
+      accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY || "",
+      secretAccessKey: process.env.S3_SECRET_KEY || "",
+    },
+    region: process.env.NEXT_PUBLIC_S3_REGION,
+    endpoint: process.env.NEXT_PUBLIC_S3_ENDPOINT,
+    forcePathStyle: true,
+  },
+  bucket: process.env.NEXT_PUBLIC_S3_BUCKET || "",
+  authorized: async (req, _res) => {
+    try {
+      const user = await isAuthorized(req);
+      return user && user.verified;
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  },
+});
