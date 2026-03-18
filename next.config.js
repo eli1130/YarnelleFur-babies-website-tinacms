cd15991c2f3dcac64d3cc5622d2f2ed1acb52974
eli1130 <e.yarnelle@gmail.com>
2026-03-18 19:18:18 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['f005.backblazeb2.com'],
+    domains: [
+      'res.cloudinary.com',
+      'pub-e847384c23164145a930ab957dbde017.r2.dev',
+      'yarnelle-fur-babies.c1af431a9062578c7d188fc2e15d738f.r2.cloudflarestorage.com'
+    ],
   },
   transpilePackages: ['next-tinacms-s3'],
   experimental: { serverActions: true },
