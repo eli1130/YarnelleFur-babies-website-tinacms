fbc36bd6c956c07737f7453c328e68e3482a4d58
eli1130 <e.yarnelle@gmail.com>
2026-04-08 17:03:35 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-LTYFZCWQM7"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-LTYFZCWQM7');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
