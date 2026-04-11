612e87c64cb8f9b7e065b9cf884c179c30daadfd
eli1130 <e.yarnelle@gmail.com>
2026-04-11 12:00:17 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import Script from 'next/script'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-512.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-LTYFZCWQM7"
         strategy="afterInteractive"
