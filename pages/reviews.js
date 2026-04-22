0dfb620c03ec3ab98bf416a49c169aab39247e70
eli1130 <e.yarnelle@gmail.com>
2026-04-21 20:42:05 -0400

    Update reviews.js

--- pages/reviews.js
+++ pages/reviews.js
@@ -94,6 +94,7 @@ export default function Reviews() {
           <li><Link href="/about">About Us</Link></li>
           <li><Link href="/gotjesus">Got Jesus?</Link></li>
           <li><Link href="/#litters">Available Litters</Link></li>
+          <li><Link href="/upcoming-litters">Upcoming Litters &amp; Waitlist</Link></li>
           <li><Link href="/reviews" className="active">Reviews</Link></li>
           <li><Link href="/facility">Our Facility</Link></li>
           <li><Link href="/faq">FAQ &amp; Products</Link></li>
@@ -134,13 +135,14 @@ export default function Reviews() {
             <Link href="/about">About Us</Link>
             <Link href="/gotjesus">Got Jesus?</Link>
             <Link href="/#litters">Available Litters</Link>
+            <Link href="/upcoming-litters">Upcoming Litters &amp; Waitlist</Link>
             <Link href="/reviews">Reviews</Link>
             <Link href="/facility">Our Facility</Link>
             <Link href="/faq">FAQ &amp; Products</Link>
           </div>
           <div className="footer-col">
             <h5>Contact</h5>
-            <p>Brooke Labenz</p><a href="tel:2604439035">(260) 443-9035</a>
+            <p>Eli Yarnelle</p><a href="tel:2604454378">(260) 445-4378</a>
             <a href="mailto:yarnellefurbabies@gmail.com">yarnellefurbabies@gmail.com</a>
             <br /><p>John Yarnelle</p><a href="tel:2604107925">(260) 410-7925</a>
           </div>
