a5e708cbe2fbb42a4bb21b078b00cc2b5c7aaa67
eli1130 <e.yarnelle@gmail.com>
2026-09-05 19:41:39 -0400

    Update reviews.js

--- pages/reviews.js
+++ pages/reviews.js
@@ -93,7 +93,7 @@ export default function Reviews() {
           <li><Link href="/">Home</Link></li>
           <li><Link href="/about">About Us</Link></li>
           <li><Link href="/gotjesus">Got Jesus?</Link></li>
-          <li><Link href="/#litters">Available Litters</Link></li>
+          <li><Link href="/litters">Available Litters</Link></li>
           <li><Link href="/upcoming-litters">Upcoming Litters &amp; Waitlist</Link></li>
           <li><Link href="/reviews" className="active">Reviews</Link></li>
           <li><Link href="/facility">Our Facility</Link></li>
@@ -134,7 +134,7 @@ export default function Reviews() {
             <Link href="/">Home</Link>
             <Link href="/about">About Us</Link>
             <Link href="/gotjesus">Got Jesus?</Link>
-            <Link href="/#litters">Available Litters</Link>
+            <Link href="/litters">Available Litters</Link>
             <Link href="/upcoming-litters">Upcoming Litters &amp; Waitlist</Link>
             <Link href="/reviews">Reviews</Link>
             <Link href="/facility">Our Facility</Link>
