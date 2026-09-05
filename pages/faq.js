24e27c5d9aa06ad30306ff1b1878d0ef632eb067
eli1130 <e.yarnelle@gmail.com>
2026-09-05 19:40:44 -0400

    Update faq.js

--- pages/faq.js
+++ pages/faq.js
@@ -186,7 +186,7 @@ export default function FAQ() {
           <li><Link href="/">Home</Link></li>
           <li><Link href="/about">About Us</Link></li>
           <li><Link href="/gotjesus">Got Jesus?</Link></li>
-          <li><Link href="/#litters">Available Litters</Link></li>
+          <li><Link href="/litters">Available Litters</Link></li>
           <li><Link href="/upcoming-litters">Upcoming Litters &amp; Waitlist</Link></li>
           <li><Link href="/reviews">Reviews</Link></li>
           <li><Link href="/facility">Our Facility</Link></li>
@@ -258,7 +258,7 @@ export default function FAQ() {
             <Link href="/">Home</Link>
             <Link href="/about">About Us</Link>
             <Link href="/gotjesus">Got Jesus?</Link>
-            <Link href="/#litters">Available Litters</Link>
+            <Link href="/litters">Available Litters</Link>
             <Link href="/upcoming-litters">Upcoming Litters &amp; Waitlist</Link>
             <Link href="/reviews">Reviews</Link>
             <Link href="/facility">Our Facility</Link>
