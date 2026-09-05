98fa4a8dab5bb732f8c3454da7a97961ab7bf668
eli1130 <e.yarnelle@gmail.com>
2026-09-05 19:41:08 -0400

    Update gotjesus.js

--- pages/gotjesus.js
+++ pages/gotjesus.js
@@ -97,7 +97,7 @@ export default function GotJesus() {
           <li><Link href="/">Home</Link></li>
           <li><Link href="/about">About Us</Link></li>
           <li><Link href="/gotjesus" className="active">Got Jesus?</Link></li>
-          <li><Link href="/#litters">Available Litters</Link></li>
+          <li><Link href="/litters">Available Litters</Link></li>
           <li><Link href="/upcoming-litters">Upcoming Litters &amp; Waitlist</Link></li>
           <li><Link href="/reviews">Reviews</Link></li>
           <li><Link href="/facility">Our Facility</Link></li>
@@ -150,7 +150,7 @@ export default function GotJesus() {
             <Link href="/">Home</Link>
             <Link href="/about">About Us</Link>
             <Link href="/gotjesus">Got Jesus?</Link>
-            <Link href="/#litters">Available Litters</Link>
+            <Link href="/litters">Available Litters</Link>
             <Link href="/upcoming-litters">Upcoming Litters &amp; Waitlist</Link>
             <Link href="/reviews">Reviews</Link>
             <Link href="/facility">Our Facility</Link>
